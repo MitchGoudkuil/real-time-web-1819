52dc2d51eddd52b8e70b383c21e2c2351c0e1f72
Mitch <mitch_goudkuil@hotmail.com>
2019-04-25 16:05:31 +0200

    config added

---
+++ mumble-run/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  consumer_key: "H2i6BNioo8JUEHEkGVx44Z7KA",
+  consumer_secret: "gE4M127vZq2bbj536ZktIiupCzX07SmA0NaVKdukZA42vllgPE",
+  access_token: "145722618-rPydwYR5VqkOCu9BBHIF7JI5WlDpb1hvoRQ6wohT",
+  access_token_secret: "76mAjewqFLtXHDDVZVWVkA7km5LgsqEwjCrIAWTpwvI9a",
+  timeout_ms: 60 * 1000, // optional HTTP request timeout to apply to all requests.
+  strictSSL: true // optional - requires SSL certificates to be valid.
+};
