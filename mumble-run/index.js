2cab5244adc88c12183ff0753f8974a9028155e6
Mitch <mitch_goudkuil@hotmail.com>
2019-04-25 15:43:55 +0200

    Added twitter api and racing feature

--- mumble-run/index.js
+++ mumble-run/index.js
@@ -6,11 +6,37 @@ const http = require("http").Server(app);
 const io = require("socket.io")(http, { pingInterval: 500 });
 const port = process.env.PORT || 3000;
 
+let pumpArray = [];
+let cardiBArray = [];
+let sixnineArray = [];
+let tentacionArray = [];
+let twentyoneArray = [];
+let futureArray = [];
+let userIDs = [];
+let timer = 5;
+
+let gambleArr = [];
+
+const TwitterStreamChannels = require("twitter-stream-channels");
+const Twit = require("twit");
+const config = require("./config");
+
+const client = new TwitterStreamChannels(config);
+
+const channels = {
+  lilpump: ["lilpump", "#lilpump", "@lilpump"],
+  cardib: ["#cardiB", "cardib", "cardiB", "@iamcardib"],
+  sixnine: ["6ix9ine", "sixnine", "@TheRiskOffic", "#6ix9ine"],
+  tentacion: ["XXXTentacion", "@xxxtentacion", "#XXXTentacion", "laptop"],
+  twentyone: ["21savage", "@21savage", "#21savage", "dennis", ":)"]
+};
+
+let stream = client.streamChannels({ track: channels });
+
 let totalConnections = [];
 let urlEncodedParser = bodyParser.urlencoded({ extended: false });
 
 app.use(express.static(__dirname + "/server/public/"));
-
 app.engine(
   "hbs",
   hbs({
@@ -30,27 +56,94 @@ app.get("/scores", function(req, res) {
   res.render("scores");
 });
 
+startgame();
+
+function startgame() {
+  let mainInterval = setInterval(function() {
+    if (totalConnections.length >= 1) {
+      timer = timer - 1;
+      console.log(timer);
+      if (timer === 0) {
+        clearInterval(mainInterval);
+        timer = 5;
+        pumpArray = [];
+        sixnineArray = [];
+        cardiBArray = [];
+        tentacionArray = [];
+        twentyoneArray = [];
+        io.sockets.emit("timer", timer);
+        stream.on("channels/lilpump", function(tweet) {
+          pumpArray.push(tweet);
+        });
+        stream.on("channels/sixnine", function(tweet) {
+          sixnineArray.push(tweet);
+        });
+        stream.on("channels/cardib", function(tweet) {
+          cardiBArray.push(tweet);
+        });
+        stream.on("channels/tentacion", function(tweet) {
+          tentacionArray.push(tweet);
+        });
+        stream.on("channels/twentyone", function(tweet) {
+          twentyoneArray.push(tweet);
+        });
+
+        let rapperBlock = {
+          LilPump: pumpArray,
+          sixnine: sixnineArray,
+          cardib: cardiBArray,
+          tentacion: tentacionArray,
+          twentyone: twentyoneArray
+        };
+
+        let racingInterval = setInterval(function() {
+          io.sockets.emit("racing", rapperBlock);
+        }, 1000);
+      } else {
+        io.sockets.emit("timer", timer);
+      }
+    }
+  }, 1000);
+}
+
 io.on("connection", function(socket) {
   totalConnections.push(socket);
-  console.log("a user connected " + totalConnections.length);
-  io.sockets.emit("users", totalConnections.length);
+  io.sockets.emit("users", {
+    users: totalConnections.length,
+    id: socket.id
+  });
+
+  socket.on("restart", function() {
+    timer = 5;
+    startgame();
+    io.emit("restart");
+  });
 
   socket.on("message", function(msg) {
     io.emit("message", msg);
   });
 
-  // // socket money buying
-  //   socket.on("buy", function(data) {
-  //     let leftMoney = data.currentMoney - data.beds
-  //     io.emit("buy", leftMoney);
-  //   })
+  socket.on("winner", function(data) {
+    let winnerFilter = data.filter(win => {
+      return win.length >= 10;
+    });
+
+    let winner = winnerFilter[0].name;
+
+    io.sockets.emit("winner", winner);
+    data = [];
+  });
+
+  socket.on("gamble", function(data) {
+    gambleArr.push(data);
+    io.sockets.emit("gamble", gambleArr);
+  });
 
   socket.on("typing", function(data) {
     socket.broadcast.emit("typing", data);
   });
 
   socket.on("disconnect", function() {
-    console.log("user disconnected " + totalConnections.length);
     totalConnections.pop();
     io.sockets.emit("users", totalConnections.length);
   });
