73b9b11475031a40f4c420681e36e9257c4835c3
Mitch <mitch_goudkuil@hotmail.com>
2019-04-25 16:35:18 +0200

    deleted the old file

--- mumble-run/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  consumer_key: "H2i6BNioo8JUEHEkGVx44Z7KA",
-  consumer_secret: "gE4M127vZq2bbj536ZktIiupCzX07SmA0NaVKdukZA42vllgPE",
-  access_token: "145722618-rPydwYR5VqkOCu9BBHIF7JI5WlDpb1hvoRQ6wohT",
-  access_token_secret: "76mAjewqFLtXHDDVZVWVkA7km5LgsqEwjCrIAWTpwvI9a",
-  strictSSL: true // optional - requires SSL certificates to be valid.
-};
